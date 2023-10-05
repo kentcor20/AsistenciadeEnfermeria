5b72b56777523d4298463836c571decfdeff9abe
kentcor20 <kentcortes666@gmail.com>
2023-10-05 07:39:12 -0500

    Update conexion_be.php

--- conexion/conexion_be.php
+++ conexion/conexion_be.php
@@ -4,11 +4,11 @@
    $conexion = mysqli_connect("localhost",  "root",  "",  "login_register_db" );
 
    if($conexion){
-    echo 'conectado exitosamente a la base de datos';
+        echo 'conectado exitosamente a la base de datos';
    }else{
-       echo 'no se ha podido conectar a la base de datos';
+        echo 'no se ha podido conectar a la base de datos';
    } 
-
+?>
 
 
 
