87d00c7a75ac8574bc715cd13e4861f3c7cbaeeb
juancaicedodelosrios <juan.caicedod@cun.edu.co>
2023-10-12 08:03:37 -0500

    Update conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,13 +1,13 @@
 <?php
 
 
-   $conexion = mysqli_connect("localhost",  "root",  "",  "login_register_db" );
+     $conexion = mysqli_connect("localhost",  "root",  "",  "login_register_db" );
 
-   if($conexion){
-        echo 'conectado exitosamente a la base de datos';
-   }else{
-        echo 'no se ha podido conectar a la base de datos';
-   } 
+     if($conexion){
+          echo 'conectado exitosamente a la base de datos';
+     }else{
+          echo 'no se ha podido conectar a la base de datos';
+     } 
 ?>
 
 
