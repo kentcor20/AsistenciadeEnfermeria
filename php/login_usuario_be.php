328e2a8abf62a31188ba235fd79dbb45c6817e14
kentcor20 <kentcortes666@gmail.com>
2023-10-19 08:36:19 -0500

    Update login_usuario_be.php

---
+++ php/login_usuario_be.php
@@ -0,0 +1,24 @@
+<?php
+    
+    include 'conexion_be.php'
+
+
+    $correo = $_POST['correo'];
+    $contraseña = $_POST['contraseña']
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contraseña='$contraseña'");
+
+    if(mysqli_num_rows($validar_login) > 0)^{
+        header("location: pag.html");
+        exit;
+    }else{
+        echo'
+           <script>
+               alert("usuario no existe, por favor verifique los datos introducidos");
+               window.location = "../index.php"
+            </script>
+  
+        ';
+        exit;
+    }
+?>
